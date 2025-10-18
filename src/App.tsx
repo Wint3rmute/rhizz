c98365fa4b10e6bf3a50bf2db927d7025fae8683
wint3rmute <mateusz.baczek1998@gmail.com>
2025-10-18 20:07:05 +0200

    hacking

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useRef } from "react";
 import "./App.css";
 import * as Viz from "@viz-js/viz";
-import { Model, Component, graph, Port} from './Model.ts';
+import { Component, graph, Model, Port } from "./Model.ts";
+import Editor from "@monaco-editor/react";
+import { Button } from 'antd';
+import { Grid } from "antd";
+
+function Ed() {
+  return (
+    <Editor
+      height="90vh"
+      defaultLanguage="javascript"
+      defaultValue="// some comment"
+    />
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   let model = new Model();
   let component = new Component();
@@ -22,17 +33,25 @@ function App() {
 
   model.components["drone"] = component;
 
-  Viz.instance().then((viz) => {
-    const system_graph = graph(model);
-    const svg = viz.renderSVGElement(system_graph);
+  const graph_ref = useRef<HTMLParagraphElement>(null);
+  useEffect(() => {
+    Viz.instance().then((viz) => {
+      const system_graph = graph(model);
+      const svg = viz.renderSVGElement(system_graph);
 
-    let parent = document.getElementById("graph");
-    if (parent.firstChild) {
-      parent.replaceChild(svg, parent.firstChild);
-    } else {
-      parent.appendChild(svg);
-    }
-  });
+      let parent = graph_ref.current; //document.getElementById("graph");
+      if (!parent) {
+        // TODO: raise error?
+        return;
+      }
+
+      if (parent.firstChild) {
+        parent.replaceChild(svg, parent.firstChild);
+      } else {
+        parent.appendChild(svg);
+      }
+    });
+  }, [graph_ref]);
 
   return (
     <>
@@ -42,7 +61,8 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p id="graph">
+        <Button type="primary">PRESS ME</Button>
+      <p ref={graph_ref}>
         Graph here?
       </p>
     </>
