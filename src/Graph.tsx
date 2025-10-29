aef75d3fd01b1e76b45e95adfccab75c9abe0909
wint3rmute <mateusz.baczek1998@gmail.com>
2025-10-29 22:38:13 +0100

    router, Graph component

---
+++ src/Graph.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+import * as Viz from "@viz-js/viz";
+
+export function Graph({ graphviz_input }: { graphviz_input: string }) {
+  const graph_ref = useRef<HTMLParagraphElement>(null);
+  const [graph_loading_state, set_graph_loading_state] = useState<string>();
+
+  useEffect(() => {
+    Viz.instance().then((viz) => {
+      try {
+        const svg = viz.renderSVGElement(graphviz_input, { engine: "dot" });
+        console.log(graphviz_input);
+
+        const parent = graph_ref.current;
+        if (!parent) {
+          // TODO: raise error?
+          return;
+        }
+
+        if (parent.firstChild) {
+          parent.replaceChild(svg, parent.firstChild);
+        } else {
+          parent.appendChild(svg);
+        }
+        set_graph_loading_state(``);
+      } catch (e) {
+        set_graph_loading_state(`${e}`);
+      }
+    });
+  }, [graphviz_input]);
+
+  return (
+    <p ref={graph_ref}>
+      {graph_loading_state}
+    </p>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import FullEditor from "./FullEditor.tsx";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+function RouteNotFound() {
+  return (
+    <>
+      <h1>Not found...</h1>
+      Go back to the <Link to="/">home page</Link>!
+    </>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/rhizz">
+      <Routes>
+        <Route path="/" element={<FullEditor />} />
+        <Route path="/beczka" element={<FullEditor />} />
+        <Route path="*" element={<RouteNotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
