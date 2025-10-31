d59e19e460eecd6ee37ca5f74cf896b31a03fb60
wint3rmute <mateusz.baczek1998@gmail.com>
2025-11-01 00:05:33 +0100

    run tests in CICD

--- tests/model_compile_test.ts
+++ tests/model_compile_test.ts
@@ -1,8 +1,6 @@
-import { assertEquals, assert } from "jsr:@std/assert";
+import { assert } from "jsr:@std/assert@^1.0.15";
 import { rocket_model } from "../src/rocket.ts";
 import { try_parse_model } from "../src/model-parser-utils.ts";
-import { SystemModelSchema, type SystemModel } from "../src/model-syntax.ts";
-import { ModelCompilationError } from "../src/model-compiler.ts";
 
 Deno.test("simple test", () => {
   const parsing_result = try_parse_model(rocket_model);
