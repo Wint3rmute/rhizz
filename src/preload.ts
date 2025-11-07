735b2adc0bfa3dfded82b985cd185720c5c37822
wint3rmute <mateusz.baczek1998@gmail.com>
2025-11-07 17:02:48 +0100

    Local editor file watcher

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,12 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
+import { watch } from "node";
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  setTitle: (title: string) => ipcRenderer.send("set-title", title),
+  onModelFilesUpdate: (callback) =>
+    ipcRenderer.on("update-model", (_event, value) => callback(value)),
+});
