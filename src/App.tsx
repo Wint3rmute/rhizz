699e4872fe3dc818f1ead73408b7bb08c29ac36b
wint3rmute <mateusz.baczek1998@gmail.com>
2025-11-07 22:55:00 +0100

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import FullEditor from "./FullEditor.tsx";
 import App from "./Navbar.tsx";
-import { StrictMode } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RouteNotFound } from "./RouteNotFound.tsx";
 import LocalFileEditor from "./LocalFileEditor.tsx";
