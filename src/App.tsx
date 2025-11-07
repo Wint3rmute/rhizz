0a460a37565333f2a2f43bbf81ccfb1e2c073052
wint3rmute <mateusz.baczek1998@gmail.com>
2025-11-07 22:53:49 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import FullEditor from "./FullEditor.tsx";
+import App from "./Navbar.tsx";
+import { StrictMode } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RouteNotFound } from "./RouteNotFound.tsx";
+import LocalFileEditor from "./LocalFileEditor.tsx";
 
 const root = createRoot(document.body);
-root.render(<FullEditor />);
+
+root.render(
+  <StrictMode>
+    <App>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<FullEditor />} />
+          <Route path="/local" element={<LocalFileEditor />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </App>
+  </StrictMode>,
+);
--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,10 @@ import FullEditor from "./FullEditor.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RouteNotFound } from "./RouteNotFound.tsx";
 import LocalFileEditor from "./LocalFileEditor.tsx";
+import App from "./Navbar.tsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/rhizz">
-      <Routes>
-        <Route path="/" element={<FullEditor />} />
-        <Route path="/local" element={<LocalFileEditor />} />
-        <Route path="*" element={<RouteNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
