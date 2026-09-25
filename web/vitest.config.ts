cd2f4c63ea4c1d8ec4aa54c67032d4029330d9f0
Mateusz Bączek <mateusz.baczek1998@gmail.com>
2026-09-25 19:03:34 +0200

    web: formatting

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   },
   test: {
     coverage: {
-      provider: 'v8',
+      provider: "v8",
       thresholds: {
         // Requires 70% function coverage
         functions: 70,
